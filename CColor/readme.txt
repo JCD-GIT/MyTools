f1cfcf7b3829e42e7acd378647e95dc289a77103
José Carlos Duarte <duartejc2001@hotmail.com>
2017-05-07 09:18:23 -0700

    ccolor

---
+++ CColor/readme.txt
@@ -0,0 +1,5 @@
+Readme.txt
+
+* Use ccolor to change the colors of the command prompt
+
+* The select color numbers can be viewed in the title adding any parameter like: ccolor /
---
+++ CColor/src/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+
+namespace ccolor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rnd = new Random();
+            int bcor = rnd.Next(15);
+            int fcor = rnd.Next(7);
+
+            if (bcor <= 7) fcor = fcor + 8;
+
+            Console.BackgroundColor = (ConsoleColor)bcor;
+            Console.ForegroundColor = (ConsoleColor)fcor;
+
+            Console.Clear();
+            Console.Title = "F="+fcor+" B="+bcor;
+            if (args.Length!=0) Console.ReadKey();
+
+        }
+    }
+}
